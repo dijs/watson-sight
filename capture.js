5afdc3456dc02e7f1598a8963bbda29bee33011d
Richard van der Dys <richard.vanderdys@gmail.com>
2017-11-01 17:10:23 +0200

    floor fix

--- capture.js
+++ capture.js
@@ -3,8 +3,8 @@ const fs = require('fs');
 function capture(path, { label, x, y, w, h }) {
   const left = Math.floor(x - w / 2);
   const top = Math.floor(y - h / 2);
-  const right = left + w;
-  const bottom = top + h;
+  const right = Math.floor(left + w);
+  const bottom = Math.floor(top + h);
   const toFile = `./captures/${label}_${Date.now()}_${left}_${right}_${top}_${bottom}.png`;
   fs.renameSync(path, toFile);
 }
